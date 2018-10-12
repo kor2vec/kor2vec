fe81ec36000d336d38d2f88e2f429f6866f1791f
codertimo <codertimo@gmail.com>
2018-10-12 22:50:20 +0900

    Fixing auto deployment issue

--- setup.py
+++ setup.py
@@ -61,5 +61,8 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ]
+    ],
+    cmdclass={
+        'verify': VerifyVersionCommand,
+    }
 )
