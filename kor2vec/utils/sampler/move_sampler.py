90024252fcdd6bfdcb499f0941ed0601c9ed4eed
codertimo <codertimo@gmail.com>
2018-10-12 22:13:16 +0900

    Change License statements

--- kor2vec/utils/sampler/move_sampler.py
+++ kor2vec/utils/sampler/move_sampler.py
@@ -1,4 +1,4 @@
-# Copyright 2018 NAVER Corp.
+# Copyright 2018 Kor2vec Contributors and NAVER Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
